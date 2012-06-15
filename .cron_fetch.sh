edc354f811a8b9e1f25a0da1a888698f10e62d8d
Rebecca Turner <turner@mikomi.org>
2012-06-15 17:42:19 -0400

    move to cron script

---
+++ .cron_fetch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+fetch_story --toc http://www.fanfiction.net/s/5782108/1/Harry_Potter_and_the_Methods_of_Rationality
+perl -pi -e's{<br/>P:.*? U:.*?</p>$}{}g' *
+git add -A
+git commit -m "$(date)"
+git push
--- .fetch_forever.sh
+++
@@ -1,8 +0,0 @@
-while true; do
-	sleep 86400
-	fetch_story --toc http://www.fanfiction.net/s/5782108/1/Harry_Potter_and_the_Methods_of_Rationality
-	perl -pi -e's{<br/>P:.*? U:.*?</p>$}{}g' *
-	git add -A
-	git commit -m "$(date)"
-	git push
-done
