b0e6a16a5920d037e7e897d7435218c16c989bbc
Rebecca Turner <turner@mikomi.org>
2012-06-18 15:39:36 -0400

    use bash, silly me

--- .cron_fetch.sh
+++ .cron_fetch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/bash
 source ~/.profile
 fetch_story --toc http://www.fanfiction.net/s/5782108/1/Harry_Potter_and_the_Methods_of_Rationality
 perl -pi -e's{<br/>P:.*? U:.*?</p>$}{}g' *
