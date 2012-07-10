f2dbd8d80ecd9940027d3db0e5befd10e456cb20
Rebecca Turner <turner@mikomi.org>
2012-07-10 18:59:08 -0400

    Only add newlines to BRs that are not at the ends of lines already

--- .cron_fetch.sh
+++ .cron_fetch.sh
@@ -4,7 +4,7 @@ source ~/.profile
 fetch_story --toc http://www.fanfiction.net/s/5782108/1/Harry_Potter_and_the_Methods_of_Rationality
 perl -pi -e's{<br/>P:.*? U:.*?</p>$}{}g' *
 perl -pi -e's{(</p>)(<p>)}{$1\n$2}g' *
-perl -pi -e's{(<br/>)}{$1\n}g' *
+perl -pi -e's{(<br/>\s*[^\n])}{$1\n}g' *
 git add -A
 git commit -m "$(date)"
 git push origin master
